86f7f4324be21356160b054c6924def25db2370d
Vigneshwar <75877958+vignu10@users.noreply.github.com>
2022-01-28 20:17:44 +0530

    Time: 264 ms (5.55%), Space: 54.8 MB (5.81%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++){
+            ans^=nums[i];
+            
+            System.out.println(ans); // For debugging purpose
+        }
+        return ans;
+    }
+}
