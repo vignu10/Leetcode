7a216d89ba488bd0af50b28fb6aafb39ea40b341
Vigneshwar <75877958+vignu10@users.noreply.github.com>
2022-01-29 11:32:53 +0530

    Time: 40 ms (52.52%), Space: 69.3 MB (46.09%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,43 @@
+/**
+ * Using stack to save the increasing height index.
+ *
+ * Time Complexity: O(N) --> Each element is visited maximum twice. (Once pushed
+ * in stack and once popped for stack)
+ *
+ * Space Complexity: O(N)
+ *
+ * N = Length of the input array.
+ */
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return heights[0];
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= len; i++) {
+            while (!stack.isEmpty() && (i == len || heights[stack.peek()] >= heights[i])) {
+                int h = heights[stack.pop()];
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                /**
+                 * i-1 - left ==> This is calculating the width of the rectangle. Both ith and
+                 * left positions are excluded.
+                 */
+                maxArea = Math.max(maxArea, (i - 1 - left) * h);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
