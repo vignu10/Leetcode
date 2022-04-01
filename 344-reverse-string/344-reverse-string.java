41b8bd6c95d3fcea6062c3a7dfa7921038613766
Vigneshwar <75877958+vignu10@users.noreply.github.com>
2022-04-01 21:18:01 +0530

    Time: 0 ms (100.00%), Space: 48.7 MB (98.09%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,7 +2,7 @@ class Solution {
     public void reverseString(char[] s) {
        int i=0;
         int j=s.length-1;
-        while(i<=j)
+        while(i<j)
         {
            char temp=s[i];
             s[i]=s[j];
@@ -10,6 +10,6 @@ class Solution {
             i++;
             j--;
         }
-       
+       return;
     }
 }
